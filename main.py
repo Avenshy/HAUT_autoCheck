7348fd465274460ba55c0fe7c640876ec4e9d198
Avenshy <30952834+Avenshy@users.noreply.github.com>
2021-01-17 16:10:36 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import time,json,requests,random,datetime
+import time
+import json
+import requests
+import random
+import datetime
 from campus import CampusCard
 
 def main():
+    #校内校外开关
+    mark = 1
     #定义变量
-    success,failure=[],[]
+    success,failure = [],[]
     #sectets字段录入
     phone, password, sckey = [], [], []
     #多人循环录入
@@ -19,47 +25,56 @@ def main():
 
     #提交打卡
     for index,value in enumerate(phone):
-        print("开始尝试为用户%s打卡"%(value))
+        print("开始尝试为用户%s打卡" % (userInfo['username']))
         count = 0
         while (count <= 3):
             try:
                 campus = CampusCard(phone[index], password[index])
                 token = campus.user_info["sessionId"]
-                userInfo=getUserInfo(token)
-                response = checkIn(userInfo,token)
+                userInfo = getUserInfo(token)
+                if mark == 0:
+                    response = checkIn(userInfo,token)
+                if mark == 1:
+                    ownPhone = phone[index]
+                    response = check(ownPhone,userInfo,token)
                 strTime = getNowTime()
                 if response.json()["msg"] == '成功':
                     success.append(userInfo['username'])
                     print(response.text)
-                    msg = strTime + userInfo['username']+"打卡成功"
+                    msg = strTime + userInfo['username'] + "打卡成功"
                     if index == 0:
-                        result=response
+                        result = response
                     break
                 else:
                     failure.append(userInfo['username'])
                     print(response.text)
-                    msg =  strTime + userInfo['username'] + "打卡异常"
+                    msg = strTime + userInfo['username']+ "打卡异常"
                     count = count + 1
                     if index == 0:
-                        result=response
-                    if count<=3:
-                        print('%s打卡失败，开始第%d次重试...'%(userInfo['username'],count))
+                        result = response
+                    if count <= 3:
+                        print('%s打卡失败，开始第%d次重试...' % (userInfo['username'],count))
                     time.sleep(5)
             except Exception as e:
                 print(e.__class__)
-                msg = "出现错误"
                 failure.append(userInfo['username'])
-                break
+                strTime = getNowTime()
+                msg = strTime + userInfo['username'] + "出现错误"
+                count = count + 1
+                if index == 0:
+                    result = response
+                if count <= 3:
+                    print('%s打卡出错，开始第%d次重试...' % (userInfo['username'],count))
+                time.sleep(1)
         print(msg)
         print("-----------------------")
     fail = sorted(set(failure),key=failure.index)
-    title = "成功: %s 人,失败: %s 人"%(len(success),len(fail))
+    title = "成功: %s 人,失败: %s 人" % (len(success),len(fail))
     try:
         print('主用户开始微信推送...')
         wechatPush(title,sckey[0],success,fail,result)
     except:
         print("微信推送出错！")
-        print(title,sckey[0],success,fail,result)
 
 #时间函数
 def getNowTime():
@@ -67,12 +82,84 @@ def getNowTime():
     strTime = cstTime.strftime("%H:%M:%S ")
     return strTime
 
-#打卡参数配置函数
-def getUserJson(userInfo,token):
-    #随机温度(36.2~36.8)
-    a=random.uniform(36.2,36.8)
+#信息获取函数
+def getUserInfo(token):
+    token = {'token':token}
+    sign_url = "https://reportedh5.17wanxiao.com/api/clock/school/getUserInfo"
+    #提交打卡
+    response = requests.post(sign_url, data=token)
+    return response.json()['userInfo']
+
+#校内打卡提交函数
+def checkIn(userInfo,token):
+    sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
+     #随机温度(36.2~36.8)
+    a = random.uniform(36.2,36.8)
     temperature = round(a, 1)
-    return  {
+    jsons = {
+            "businessType": "epmpics",
+            "method": "submitUpInfoSchool",
+            "jsonData": {
+            "deptStr": {
+                "deptid": userInfo['classId'],
+                "text": userInfo['classDescription']
+            },
+            #如果你来自其他学校，请自行打卡抓包修改地址字段
+            "areaStr": {"streetNumber":"","street":"长椿路辅路","district":"中原区","city":"郑州市","province":"河南省","town":"","pois":"河南工业大学(莲花街校区)","lng":113.55064699999795 + random.random() / 1000,"lat":34.83870696238093 + random.random() / 1000,"address":"中原区长椿路辅路河南工业大学(莲花街校区)","text":"河南省-郑州市","code":""},
+            "reportdate": round(time.time() * 1000),
+            "customerid": userInfo['customerId'],
+            "deptid": userInfo['classId'],
+            "source": "app",
+            "templateid": "clockSign2",
+            "stuNo": userInfo['stuNo'],
+            "username": userInfo['username'],
+            "userid": round(time.time()),
+            "updatainfo": [{
+                    "propertyname": "temperature",
+                    "value": temperature
+                },
+                {
+                    "propertyname": "symptom",
+                    "value": "无症状"
+                }],
+            "customerAppTypeRuleId": 147,
+            "clockState": 0,
+            "token": token
+            },
+            "token": token
+    }
+    #提交打卡
+    response = requests.post(sign_url, json=jsons)
+    return response
+
+#校外打卡
+def check(ownPhone,userInfo,token):
+    sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
+    #获取datajson
+    post_json = {
+            "businessType": "epmpics",
+            "jsonData": {
+            "templateid": "pneumonia",
+            "token": token
+        },
+            "method": "getUpDataInfoDetail"
+    }      
+    response = requests.post(sign_url, json=post_json).json()
+    data = json.loads(response['data'])
+    info_dict = {
+            "add":data['add'],
+            "areaStr": data['areaStr'],
+            "updatainfo": [{"propertyname": i["propertyname"], "value": i["value"]} for i in
+                            data['cusTemplateRelations']]
+        }
+    #随机温度
+    a = random.uniform(36.2,36.8)
+    temperature = round(a, 1)
+    for i in info_dict['updatainfo']: 
+        if i['propertyname'] == 'temperature':
+            i['value'] = temperature
+    #校外打卡提交json
+    check_json = {
         "businessType": "epmpics",
         "method": "submitUpInfo",
         "jsonData": {
@@ -92,7 +179,7 @@ def getUserJson(userInfo,token):
         "userid": round(time.time()),
         "updatainfo": [{
                 "propertyname": "temperature",
-                "value": "36.5"
+                "value": temperature
             }, {
                 "propertyname": "symptom",
                 "value": "无症状"
@@ -134,13 +221,13 @@ def getUserJson(userInfo,token):
                 "value": "绿色"
             }, {
                 "propertyname": "ownPhone",
-                "value": "15992646144"
+                "value": ownPhone
             }, {
                 "propertyname": "emergencyContact",
-                "value": "黄纯贵"
+                "value": userInfo['username']
             }, {
                 "propertyname": "mergencyPeoplePhone",
-                "value": "13727896670"
+                "value": ownPhone
             }, {
                 "propertyname": "assistRemark",
                 "value": ""
@@ -150,23 +237,9 @@ def getUserJson(userInfo,token):
         "token": token
         },
         "token": token
-    }    
-
-#信息获取函数
-def getUserInfo(token):
-    token={'token':token}
-    sign_url = "https://reportedh5.17wanxiao.com/api/clock/school/getUserInfo"
-    #提交打卡
-    response = requests.post(sign_url, data=token)
-    return response.json()['userInfo']
-
-#打卡提交函数
-def checkIn(userInfo,token):
-    sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
-    jsons=getUserJson(userInfo,token)
-    #提交打卡
-    response = requests.post(sign_url, json=jsons)
-    return response
+    }
+    res = requests.post(sign_url, json=check_json) 
+    return res
 
 #微信通知
 def wechatPush(title,sckey,success,fail,result):    
@@ -189,7 +262,7 @@ def wechatPush(title,sckey,success,fail,result):
             "text":title,
             "desp":content
     }
-    scurl='https://sc.ftqq.com/'+sckey+'.send'
+    scurl = 'https://sc.ftqq.com/' + sckey + '.send'
     try:
         req = requests.post(scurl,data = data)
         if req.json()["errmsg"] == 'success':
@@ -200,4 +273,5 @@ def wechatPush(title,sckey,success,fail,result):
         print("微信推送参数错误")
 
 if __name__ == '__main__':
+    mark = 1
     main()
